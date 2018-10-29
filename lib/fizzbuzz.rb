1859426a92b6f80a9368f61ad5657e5623c11af5
itsellej <daniellehallal@gmail.com>
2018-10-29 14:02:58 +0000

    return buzz for multiples of 5 but not multiples of 3 and 5

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(number)
-  if (number % 3 == 0)
+  if number % 3 == 0
     'fizz'
-  elsif number == 5
+  elsif number % 5 == 0
     'buzz'
   else
     number
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,8 @@ describe 'fizzbuzz' do
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
+
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
 end
